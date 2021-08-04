bdb900c7437b3628755d3497ac8311b5a70452e7
EC2 Default User <ec2-user@ip-172-31-94-148.ec2.internal>
2021-08-04 03:56:32 +0000

    いいね・コメント機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+
   get "home/about" => "homes#about"
   resources :homes
   post 'users' => 'users#create'
